bb998b667be3b9fdb20c163245c1605afe7c5823
Morgan Dean <morgandeanhi@gmail.com>
2026-07-08 10:34:47 -0700

    remove biome, add immich api

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,8 @@
+import { getAlbums } from "$lib/immich";
+import type { PageServerLoad } from "./$types";
+
+export const load: PageServerLoad = async () => {
+	return {
+		albums: await getAlbums(),
+	};
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,15 @@
-<h1 class="text-2xl">Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
-  documentation
-</p>
+<script lang="ts">
+import BikeMap from "../components/BikeMap.svelte";
+import type { PageProps } from "./$types";
+let { data }: PageProps = $props();
+</script>
+
+<div class="h-20">
+<h1 class="text-2xl">Morgan's Bikepacking</h1>
+  {#each data.albums as album}
+<p>{album}</p>
+{/each}
+</div>
+<div class="h-[calc(100dvh-5rem)]">
+<BikeMap />
+</div>
