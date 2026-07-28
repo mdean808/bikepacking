29a0561500f6de5f398514a54089a061f3d82920
Morgan Dean <morgandeanhi@gmail.com>
2026-07-28 01:13:08 -0700

    fix fonts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,7 +13,7 @@
 
 <div class="px-0 gap-5">
   <a href="/" class="mx-auto text-center">
-    <h1 class="text-2xl lg:text-6xl uppercase w-fit mx-auto mb-3 mt-4">
+    <h1 class="text-3xl lg:text-6xl uppercase w-fit mx-auto mb-3 mt-4">
       {#if isHome}
         Bikepacking Trips
       {:else}
@@ -22,7 +22,7 @@
     </h1>
   </a>
   {@render children()}
-  <div class="mx-auto flex flex-row max-w-5xl gap-2 py-4 sm:px-8">
+  <div class="mx-auto flex flex-row max-w-5xl gap-2 py-4 px-4 sm:px-8">
     <Badge variant="outline">Built by Morgan Dean</Badge>
     <a href="https://mogdan.xyz" target="_blank" class="ml-auto">
       <Badge variant="default" class="bg-blue-500 text-white">
